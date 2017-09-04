08ef9698c6a74ed9d41a58fc8ccd1938c5cbb7ec
sergiovillagran <sergio.villagran@3dves.com>
2017-09-04 07:29:47 -0500

    first comit: package.json, and first mongoose models definitions

---
+++ app/data/request.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var request = new Schema({
+	 
+}
+
+module.exports = mongoose.model('request', request);
---
+++ app/data/userData.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var address = new Schema({
+	_id: Schema.ObjectId
+	name:  String,
+	main: {
+		type: String,
+		number: Number,
+		name: String
+	}
+	complement: [{
+		type: String,
+		number: Number,
+		name: String
+	}]
+});
+
+var user = new Schema({
+	_id: Schema.ObjectId
+	name:  String,
+	lastName: String,
+	userName: String,
+	password: String,
+	registerDate: { type: Date, default: Date.now },
+	address: [address],
+	credentials: [String] // validate the identity of an user
+});
+
+module.exports = mongoose.model('user', user);
